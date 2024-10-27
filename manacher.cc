be68f422c8ad30693ebc51026f131943b242e132
kcy <ravilscottle@gmail.com>
2024-10-27 10:13:03 +0800

    add comment

--- manacher.cc
+++ manacher.cc
@@ -1,36 +1,44 @@
 #include <bits/stdc++.h>
 
 /**
- * s:
- *  index: 0 1 2 3 4 5
- *  value: a b a a b a
+ * Function to find the radiuses of palindromic substrings centered at each character using Manacher's algorithm.
  * 
- * str:
- *  index: 0123456789ABC
- *  value: #a#b#a#a#b#a#
+ * @param s The original input string.
+ * @return A vector containing the radius of the longest palindromic substring centered at each character of the transformed string.
  */
 
 std::vector<int> manacher(std::string &s) {
+    // Transform the original string `s` by inserting '#' between each character and at the start and end, to handle even-length palindromes uniformly.
     std::string str = "#";
     for (char &c : s) {
         str += std::string({c, '#'});
     }
 
     int m = str.size();
+
+    // Array to store the radius of the longest palindromic substring for each position.
     std::vector<int> radiuses(m, 0);
-    for (int current = 1, center = 0, right = 0; current < m; ++current) {
+
+    // Variables to keep track of the rightmost palindrome's center and its right edge.
+    int center = 0, right = 0;
+
+    for (int current = 1; current < m; ++current) {
+        // If `current` is within the range of the rightmost palindrome, use the previously calculated values to initialize `radiuses[current]`.
         if (current < right) {
             radiuses[current] = std::min(right - current, radiuses[center - (current - center)]);
         }
 
+        // Expand around the current center to find the maximum palindrome radius.
         for (int l = current - (radiuses[current] + 1), r = current + (radiuses[current] + 1); 0 <= l && r < m; --l, ++r, ++radiuses[current]) {
             if (str[l] != str[r]) {
                 break;
             }
         }
 
+        // Update the `center` and `right` if the new palindrome expands beyond `right`.
         if (right < current + radiuses[current]) {
-            center = current, right = current + radiuses[current];
+            center = current;
+            right = current + radiuses[current];
         }
     }
 
